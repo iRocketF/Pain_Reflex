e15483a4db4b8dcb806dc61a7d44adafbe86f6bd
Eero Salmi <eswsalmi@gmail.com>
2022-06-04 00:50:08 +0300

    Muzzle flash rotate

--- Assets/Muzzleflash.cs
+++ Assets/Muzzleflash.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Muzzleflash : MonoBehaviour
 {
     Animator animator;
+
+    [SerializeField] Transform smoke;
+    [SerializeField] Transform flame;
+
+    float randomZ;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.SetTrigger("Play");
+
+        randomZ = Random.Range(0, 360);
+
+        smoke.Rotate(0, 0, randomZ);
+        flame.Rotate(0, 0, randomZ);
     }
 }
